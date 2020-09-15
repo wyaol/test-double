2767b8edb007472f3dca7e338e78b7bd2aada5b3
wyao <289672494@qq.com>
2020-09-15 20:55:22 +0800

    [wang yao] SecurityCenterTest Finish

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,19 +1,36 @@
 package mock;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith({MockitoExtension.class})
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+    @Mock
+    DoorPanel mockDoorPanel;
 
-    @BeforeEach
-    public void setUp() {
-    }
+    @InjectMocks
+    SecurityCenter securityCenter;
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        // given
+
+        // when
+        securityCenter.switchOn();
+
+        //then
+        verify(mockDoorPanel, times(1)).close();
+
     }
 }
